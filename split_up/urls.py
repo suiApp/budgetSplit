e65a6bf3f029744d7d2150ea6947a66123dd2c97
richard parker <93417913+suiApp@users.noreply.github.com>
2023-01-13 11:36:51 +0000

    urls changed

--- split_up/urls.py
+++ split_up/urls.py
@@ -1,13 +1,12 @@
-
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-)
 
 urlpatterns = [
-    path('api/token',TokenObtainPairView.as_view(),name='token_pair'),
-  path('api/token/refresh',TokenRefreshView.as_view(),name="refresh_token"),
+    path('api/token', TokenObtainPairView.as_view(), name='token_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(),
+         name="refresh_token"),
 ]
